fdc20de6f8323def37ceb30f2c1ee8308677c22a
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-04-20 19:02:33 +0300

    a

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,6 +1,6 @@
 import { Navbar, Nav } from 'react-bootstrap';
 const MyNavbar = () => (
-    <Navbar bg="dark" variant="dark">
+  <Navbar bg="dark" variant="dark">
     <Navbar.Brand href="#">Bookstore</Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="#">Home</Nav.Link>
